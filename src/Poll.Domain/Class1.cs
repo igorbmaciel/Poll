8627e4614a47e8358860f5e3b8c4d7603ca54a9d
igor.maciel <igor.maciel@d0007azurecloudtotvscom.onmicrosoft.com>
2020-11-05 14:35:10 -0300

    feat(structure): add structure project

---
+++ src/Poll.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Poll.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/Poll.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Poll.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
