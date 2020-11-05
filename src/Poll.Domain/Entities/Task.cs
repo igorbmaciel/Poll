116eb305dce5695e44e5763b66eace1d2b21b611
igor.maciel <igor.maciel@d0007azurecloudtotvscom.onmicrosoft.com>
2020-11-05 17:46:43 -0300

    feat(domain): add domain entities

---
+++ src/Poll.Domain/Entities/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Poll.Domain.Entities
+{
+    public class Task
+    {
+        public Task()
+        {
+
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Task(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;           
+        }
+
+        public Task(Guid id, string name)
+        {
+            Id = id;
+            Name = name;           
+        }
+
+        internal static Task AddTask(string name)
+        {
+            return new Task(name);
+        }
+
+        internal static Task ChangeTask(Guid id, string name)
+        {
+            return new Task(id, name);
+        }
+    }
+}
