e5341fb39f2d8a70560cd451269cee08724460b0
igor.maciel <igor.maciel@d0007azurecloudtotvscom.onmicrosoft.com>
2020-11-07 18:05:35 -0300

    feat(employee): add infra

---
+++ src/Poll.Infra/Mappers/EmployeeMapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Poll.Domain.Entities;
+
+namespace Poll.Infra.Mappers
+{
+    public class EmployeeMapper : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> mapper)
+        {
+            mapper.ToTable("Employee");
+
+            mapper.Property(e => e.Id)
+                .HasAnnotation("Relational:ColumnName", "EmployeeId")
+                .ValueGeneratedOnAdd();
+
+            mapper.HasKey(e => e.Id);
+
+            mapper.Property(e => e.Name)
+                .HasMaxLength(150)
+                .IsRequired();
+         
+            mapper.Property(e => e.Password)
+              .IsRequired();          
+
+            mapper.HasIndex(e =>  e.Id)
+                 .IsUnique();
+        }
+    }
+}
