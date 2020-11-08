f45885b17e0c3cd5e58c9882a874d9808be5eb28
igor.maciel <igor.maciel@d0007azurecloudtotvscom.onmicrosoft.com>
2020-11-08 15:33:15 -0300

    fix(get): ajust get

--- backend/Poll/src/Poll.Domain/Queries/Response/GetTasksVotesResponse.cs
+++ backend/Poll/src/Poll.Domain/Queries/Response/GetTasksVotesResponse.cs
@@ -1,12 +1,10 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Poll.Domain.Queries.Response
 {
     public class GetTasksVotesResponse
     {
-        [JsonIgnore]
         public Guid TaskId { get; set; }
         public string TaskName { get; set; }
         public List<GetEmployeeVotesResponse> EmployeeVotes { get; set; }
